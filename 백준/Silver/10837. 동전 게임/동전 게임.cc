ba1cd1aa80cfaecc37f176f94e079f869d26c536
jy7960 <123443418+jy7960@users.noreply.github.com>
2026-01-09 15:54:56 +0900

    [Silver II] Title: 동전 게임, Time: 16 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Silver/10837. 동전 게임/동전 게임.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main(void) {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    int K;
+    cin >> K;
+
+    int C;
+    cin >> C;
+
+    for (int c = 0; c < C; c++) {
+        int M, N;
+        cin >> M >> N;
+
+        // 영희와 동수 점수가 같다면 100% 가능
+        if (M == N) {
+            cout << 1 << "\n";
+            continue;
+        }
+
+        int remainGames = K - max(M, N);
+        int diff = abs(M - N);
+
+        // 영희 점수가 더 높을 경우
+        if (M > N) {
+            if (diff - remainGames <= 2) {
+                cout << 1 << "\n";
+            } else {
+                cout << 0 << "\n";
+            }
+            continue;
+        }
+
+        // 동수 점수가 더 높을 경우
+        if (diff - remainGames <= 1) {
+            cout << 1 << "\n";
+        } else {
+            cout << 0 << "\n";
+        }
+    }
+
+    return 0;
+}
